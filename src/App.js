24b099d47a0f91c067145a45f57adb2be0a78023
tri.zn <trizn610@gmail.com>
2021-07-09 18:17:42 +0700

    add job deploy, fix ssh key, test

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello ReactJS!
+          Hello ReactJS!sdasdsdad
         </p>
-        <h1> Add Youtube </h1>
+        <h1> Add Yodsdutube </h1>
       </header>
     </div>
   );
